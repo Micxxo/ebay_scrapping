7550b19bd25a7a70beb750707ff534c85c161e95
Mico Febrian <sutanmico@gmail.com>
2025-02-07 16:04:49 +0700

    refactor: handle null at url parser

--- utils/urlParserHelper.js
+++ utils/urlParserHelper.js
@@ -2,7 +2,7 @@ const { getURL } = require("../config/index");
 
 const handleUrlParser = (params) => {
   const { query, page, limit } = params;
-  return getURL(query || "nike", Number(limit) || 60, Number(page) || 1);
+  return getURL(query ?? "nike", Number(limit) ?? 60, Number(page) ?? 1);
 };
 
 module.exports = handleUrlParser;
